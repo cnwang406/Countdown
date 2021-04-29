baff13e69d603a09bdbb1450dd039fb27171ab34
cnwang <cnwang406@gmail.com>
2021-04-29 22:24:06 +0800

    Initial Commit

---
+++ Countdown/CountdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountdownApp.swift
+//  Countdown
+//
+//  Created by cnwang on 2021/4/29.
+//
+
+import SwiftUI
+
+@main
+struct CountdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
