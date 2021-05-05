16d0420eb5895898d00f7a0834a24e91a1ee908c
cnwang <cnwang406@gmail.com>
2021-05-06 06:08:19 +0800

    widget fix. forget to save()

--- Countdown/View/DateDetailView.swift
+++ Countdown/View/DateDetailView.swift
@@ -13,33 +13,35 @@ struct DateDetailView: View {
     //MARK: - BODY
     var body: some View {
         VStack{
-        HStack {
-//            Text ("From")
+            HStack {
+                //            Text ("From")
                 Spacer()
-//            Text(countdownVM.dateFrom.toString())
-            DatePicker("Start from", selection: $countdownVM.dateFrom)
-                .foregroundColor(countdownVM.dateFromEnabled ? .blue : .gray)
-                .onTapGesture(perform: {
-                    countdownVM.dateFromEnabled.toggle()
-                    if !countdownVM.dateToEnabled && !countdownVM.dateFromEnabled {
+                //            Text(countdownVM.dateFrom.toString())
+                DatePicker("Start from", selection: $countdownVM.dateFrom)
+                    .foregroundColor(countdownVM.dateFromEnabled ? .blue : .gray)
+                    .onTapGesture(perform: {
                         countdownVM.dateFromEnabled.toggle()
+                        if !countdownVM.dateToEnabled && !countdownVM.dateFromEnabled {
+                            countdownVM.dateFromEnabled.toggle()
+                        }
+                        print ("reload timeline")
+                        countdownVM.save()
+                        WidgetCenter.shared.reloadAllTimelines()
+                    })
+                    .onChange(of: countdownVM.dateFrom) { Equatable in
+                        print("onChange: \(Equatable)")
+                        print ("reload timeline")
+                        countdownVM.save()
+                        WidgetCenter.shared.reloadAllTimelines()
                     }
-                    print ("reload timeline")
-                    WidgetCenter.shared.reloadAllTimelines()
-                })
-                .onChange(of: countdownVM.dateFrom) { Equatable in
-                    print("onChange: \(Equatable)")
-                    print ("reload timeline")
-                    WidgetCenter.shared.reloadAllTimelines()
-                }
-            
-            
-        }
+                
+                
+            }
             Spacer()
             HStack {
-//                Text ("To")
+                //                Text ("To")
                 Spacer()
-//                Text(countdownVM.dateTo.toString())
+                //                Text(countdownVM.dateTo.toString())
                 DatePicker("End at", selection: $countdownVM.dateTo)
                     .foregroundColor(countdownVM.dateToEnabled ? .blue : .gray)
                     .onTapGesture(perform: {
@@ -48,17 +50,22 @@ struct DateDetailView: View {
                             countdownVM.dateToEnabled.toggle()
                         }
                         print ("reload timeline")
+                        countdownVM.save()
                         WidgetCenter.shared.reloadAllTimelines()
+                        
                     })
                     .onChange(of: countdownVM.dateTo) { Equatable in
                         print ("reload timeline")
+                        countdownVM.save()
                         WidgetCenter.shared.reloadAllTimelines()
                     }
             }
+            Text("\(countdownVM.dateFrom.description)")
+            Text("\(countdownVM.dateTo.description)")
             
         }
         .padding()
-        .frame(width: 350, height: 90, alignment: .center)
+        .frame(width: 350, height: 110, alignment: .center)
         
     }
 }
--- CountdownWidget/CountdownWidget.swift
+++ CountdownWidget/CountdownWidget.swift
@@ -12,15 +12,15 @@ struct Provider: TimelineProvider {
     func placeholder(in context: Context) -> SimpleEntry {
         SimpleEntry(date: Date())
     }
-
+    
     func getSnapshot(in context: Context, completion: @escaping (SimpleEntry) -> ()) {
         let entry = SimpleEntry(date: Date())
         completion(entry)
     }
-
+    
     func getTimeline(in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
         var entries: [SimpleEntry] = []
-
+        
         // Generate a timeline consisting of five entries an hour apart, starting from the current date.
         let currentDate = Date()
         for minuteOffset in 0 ..< 5 {
@@ -28,7 +28,7 @@ struct Provider: TimelineProvider {
             let entry = SimpleEntry(date: entryDate)
             entries.append(entry)
         }
-
+        
         let timeline = Timeline(entries: entries, policy: .atEnd)
         completion(timeline)
     }
@@ -44,7 +44,7 @@ struct CountdownWidgetEntryView : View {
     var entry: Provider.Entry
     @Environment(\.widgetFamily) var widgetFamily
     @StateObject var countdownVM = CountdownViewModel()
-    @State var counter: Int = 0
+    
     var body: some View {
         ZStack{
             Image("tesla")
@@ -52,12 +52,19 @@ struct CountdownWidgetEntryView : View {
                 .scaledToFit().opacity(0.3)
             WidgetProgressView()
                 .environmentObject(countdownVM)
-            HStack {
-                Text(entry.date, style: .time)
-                Text("\(counter)")
-            }.opacity(0.2)
-            .offset(y:60)
-        }
+            if widgetFamily == .systemLarge {
+                HStack {
+                    Text(entry.date, style: .time)
+                    Text(entry.date.description)
+                    VStack{
+                    Text("\(countdownVM.dateFrom.description)")
+                    Text("\(countdownVM.dateTo.description)")
+                    }
+//                        .font(.footnote)
+                }.opacity(0.9)
+                .offset(y: 90)
+            }
+        } //:ZStack
         .onAppear(perform: {
             print("Widget onAppear")
         })
@@ -68,7 +75,7 @@ struct CountdownWidgetEntryView : View {
 @main
 struct CountdownWidget: Widget {
     let kind: String = "CountdownWidget"
-
+    
     var body: some WidgetConfiguration {
         StaticConfiguration(kind: kind, provider: Provider()) { entry in
             CountdownWidgetEntryView(entry: entry)
@@ -84,13 +91,13 @@ struct CountdownWidget_Previews: PreviewProvider {
         Group {
             CountdownWidgetEntryView(entry: SimpleEntry(date: Date()))
                 .previewContext(WidgetPreviewContext(family: .systemSmall))
-//                .environmentObject(countdownVM)
-//            CountdownWidgetEntryView(entry: SimpleEntry(date: Date()))
-//                .previewContext(WidgetPreviewContext(family: .systemMedium))
-////                .environmentObject(countdownVM)
-//            CountdownWidgetEntryView(entry: SimpleEntry(date: Date()))
-//                .previewContext(WidgetPreviewContext(family: .systemLarge))
-//                .environmentObject(countdownVM)
+            //                .environmentObject(countdownVM)
+                        CountdownWidgetEntryView(entry: SimpleEntry(date: Date()))
+                            .previewContext(WidgetPreviewContext(family: .systemMedium))
+            ////                .environmentObject(countdownVM)
+                        CountdownWidgetEntryView(entry: SimpleEntry(date: Date()))
+                            .previewContext(WidgetPreviewContext(family: .systemLarge))
+//                            .environmentObject(countdownVM)
         }
     }
 }
